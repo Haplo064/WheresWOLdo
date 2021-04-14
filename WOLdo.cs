8e8619a5696d00919c3cbeea5babebaa7146245b
Haplo064 <haplo064@gmail.com>
2021-04-14 13:51:56 +0930

    API 3 update
    - Also code revision

--- WOLdo.cs
+++ WOLdo.cs
@@ -10,181 +10,178 @@ namespace WOLdo
     public class WOLdo : IDalamudPlugin
     {
         public string Name => "WOLdo";
-        private DalamudPluginInterface pi;
-        public Config Configuration;
-        public string location = "";
-        public bool enabled = true;
-        public Lumina.Excel.ExcelSheet<TerritoryType> terr;
-        public float scale = 1f;
-        public ImColor col = new ImColor { Value = new Num.Vector4(1f, 1f, 1f, 1f) };
-        public bool no_move = false;
-        public bool config = false;
-        public bool debug = false;
-        public int align = 0;
-        public string[] alignStr = { "Left", "Middle", "Right" };
-        public float adjustX = 0;
-        public bool first = true;
+        private DalamudPluginInterface _pi;
+        private Config _configuration;
+        private string _location = "";
+        private bool _enabled = true;
+        private Lumina.Excel.ExcelSheet<TerritoryType> _terr;
+        private float _scale = 1f;
+        private ImColor _col = new ImColor { Value = new Num.Vector4(1f, 1f, 1f, 1f) };
+        private bool _noMove;
+        private bool _config;
+        private bool _debug;
+        private int _align;
+        private float _adjustX;
+        private bool _first = true;
 
         public void Initialize(DalamudPluginInterface pi)
         {
 
-            this.pi = pi;
-            Configuration = pi.GetPluginConfig() as Config ?? new Config();
-            col = Configuration.Col;
-            no_move = Configuration.NoMove;
-            scale = Configuration.Scale;
-            enabled = Configuration.Enabled;
-            align = Configuration.Align;
-            terr = pi.Data.GetExcelSheet<TerritoryType>();
+            _pi = pi;
+            _configuration = pi.GetPluginConfig() as Config ?? new Config();
+            _col = _configuration.Col;
+            _noMove = _configuration.NoMove;
+            _scale = _configuration.Scale;
+            _enabled = _configuration.Enabled;
+            _align = _configuration.Align;
+            _terr = pi.Data.GetExcelSheet<TerritoryType>();
 
-            this.pi.UiBuilder.OnBuildUi += DrawWindow;
+            _pi.UiBuilder.OnBuildUi += DrawWindow;
 
-            this.pi.CommandManager.AddHandler("/woldo", new Dalamud.Game.Command.CommandInfo(Command)
+            _pi.CommandManager.AddHandler("/woldo", new Dalamud.Game.Command.CommandInfo(Command)
             {
                 HelpMessage = "Where's WOLdo config."
             });
         }
         public void Dispose()
         {
-            this.pi.UiBuilder.OnBuildUi -= DrawWindow;
-            this.pi.CommandManager.RemoveHandler("/woldo");
-            terr = null;
+            _pi.UiBuilder.OnBuildUi -= DrawWindow;
+            _pi.CommandManager.RemoveHandler("/woldo");
+            _terr = null;
         }
 
-        public void Command(string command, string arguments)
+        private void Command(string command, string arguments)
         {
-            config = true;
+            _config = true;
         }
 
         private void DrawWindow()
         {
 
-            ImGuiWindowFlags window_flags = 0;
-            window_flags |= ImGuiWindowFlags.NoTitleBar;
-            window_flags |= ImGuiWindowFlags.NoScrollbar;
-            window_flags |= ImGuiWindowFlags.NoScrollbar;
-            if (no_move)
+            ImGuiWindowFlags windowFlags = 0;
+            windowFlags |= ImGuiWindowFlags.NoTitleBar;
+            windowFlags |= ImGuiWindowFlags.NoScrollbar;
+            windowFlags |= ImGuiWindowFlags.NoScrollbar;
+            if (_noMove)
             {
-                window_flags |= ImGuiWindowFlags.NoMove;
-                window_flags |= ImGuiWindowFlags.NoMouseInputs;
-                window_flags |= ImGuiWindowFlags.NoNav;
+                windowFlags |= ImGuiWindowFlags.NoMove;
+                windowFlags |= ImGuiWindowFlags.NoMouseInputs;
+                windowFlags |= ImGuiWindowFlags.NoNav;
             }
-            window_flags |= ImGuiWindowFlags.AlwaysAutoResize;
-            if (!debug)
+            windowFlags |= ImGuiWindowFlags.AlwaysAutoResize;
+            if (!_debug)
             {
-                window_flags |= ImGuiWindowFlags.NoBackground;
+                windowFlags |= ImGuiWindowFlags.NoBackground;
             }
 
 
-            if (config)
+            if (_config)
             {
                 
                 ImGui.SetNextWindowSize(new Num.Vector2(200, 160), ImGuiCond.FirstUseEver);
-                ImGui.Begin("Where's WOLdo Config", ref config, ImGuiWindowFlags.AlwaysAutoResize);
-                ImGui.Checkbox("Enabled", ref enabled);
-                ImGui.ColorEdit4("Colour", ref col.Value, ImGuiColorEditFlags.NoInputs);
-                ImGui.InputFloat("Size", ref scale);
-                ImGui.Checkbox("Locked", ref no_move);
-                ImGui.Checkbox("Debug", ref debug);
+                ImGui.Begin("Where's WOLdo Config", ref _config, ImGuiWindowFlags.AlwaysAutoResize);
+                ImGui.Checkbox("Enabled", ref _enabled);
+                ImGui.ColorEdit4("Colour", ref _col.Value, ImGuiColorEditFlags.NoInputs);
+                ImGui.InputFloat("Size", ref _scale);
+                ImGui.Checkbox("Locked", ref _noMove);
+                ImGui.Checkbox("Debug", ref _debug);
                 //ImGui.ListBox("Alignment", ref align, alignStr, 3);
 
                 if (ImGui.Button("Save and Close Config"))
                 {
                     SaveConfig();
-                    config = false;
+                    _config = false;
                 }
                 ImGui.End();
             }
 
-            location = "";
-            if (pi.ClientState.LocalPlayer != null)
+            _location = "";
+            if (_pi.ClientState.LocalPlayer != null)
             {
-                location = "Uhoh";
+                _location = "Uhoh";
                 try
                 {
-                    location = terr.GetRow(pi.ClientState.TerritoryType).PlaceName.Value.Name;
+                    _location = _terr.GetRow(_pi.ClientState.TerritoryType).PlaceName.Value.Name;
                 }
 
-                catch (Exception e)
+                catch (Exception)
                 {
-                    location = "Change zone to load";
+                    _location = "Change zone to load";
                 }
                 
                             
             }
 
 
-            if (enabled)
+            if (!_enabled) return;
+            ImGui.PushStyleColor(ImGuiCol.Text, _col.Value);
+            ImGui.Begin("WOLdo", ref _enabled, windowFlags);
+            ImGui.SetWindowFontScale(_scale);
+                
+            if (_debug)
             {
-                ImGui.PushStyleColor(ImGuiCol.Text, col.Value);
-                ImGui.Begin("WOLdo", ref enabled, window_flags);
-                ImGui.SetWindowFontScale(scale);
-                
-                if (debug)
+                ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X + _adjustX, ImGui.GetWindowPos().Y));
+                if (_align == 0)
                 {
-                    ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X + adjustX, ImGui.GetWindowPos().Y));
-                    if (align == 0)
-                    {
-                        adjustX = 0;
-                        ImGui.Text("Left Align");
-                    }
-                    if (align == 1)
-                    {
-                        adjustX = (float)Math.Floor((ImGui.CalcTextSize("Middle Align").X) / 2);
-                        ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X - adjustX, ImGui.GetWindowPos().Y));
-                        ImGui.Text("Middle Align");
-                    }
-                    if (align == 2)
-                    {
-                        adjustX = ImGui.CalcTextSize("Right Align").X;
-                        ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X - adjustX, ImGui.GetWindowPos().Y));
-                        ImGui.Text("Right Align");
-                    }
+                    _adjustX = 0;
+                    ImGui.Text("Left Align");
                 }
-                else
+                if (_align == 1)
                 {
-                    ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X + adjustX, ImGui.GetWindowPos().Y));
-                    if (align == 0)
-                    {
-                        adjustX = 0;
-                    }
-                    if (align == 1)
-                    {
-                        adjustX = (float)Math.Floor((ImGui.CalcTextSize(location).X) / 2);
-                        ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X - adjustX, ImGui.GetWindowPos().Y));
-                    }
-                    if (align == 2)
-                    {
-                        adjustX = ImGui.CalcTextSize(location).X;
-
-                        if (first)
-                        {
-                            if (adjustX != 0) { first = false; }
-                        }
-                        else
-                        {
-
-                           ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X - adjustX, ImGui.GetWindowPos().Y));
-                        }
-                            
-                    }
-                    ImGui.Text(location);
-
+                    _adjustX = (float)Math.Floor((ImGui.CalcTextSize("Middle Align").X) / 2);
+                    ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X - _adjustX, ImGui.GetWindowPos().Y));
+                    ImGui.Text("Middle Align");
                 }
-                ImGui.End();
-                ImGui.PopStyleColor();
+                if (_align == 2)
+                {
+                    _adjustX = ImGui.CalcTextSize("Right Align").X;
+                    ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X - _adjustX, ImGui.GetWindowPos().Y));
+                    ImGui.Text("Right Align");
+                }
             }
+            else
+            {
+                ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X + _adjustX, ImGui.GetWindowPos().Y));
+                if (_align == 0)
+                {
+                    _adjustX = 0;
+                }
+                if (_align == 1)
+                {
+                    _adjustX = (float)Math.Floor((ImGui.CalcTextSize(_location).X) / 2);
+                    ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X - _adjustX, ImGui.GetWindowPos().Y));
+                }
+                if (_align == 2)
+                {
+                    _adjustX = ImGui.CalcTextSize(_location).X;
+
+                    if (_first)
+                    {
+                        if (Math.Abs(_adjustX) > 0) { _first = false; }
+                    }
+                    else
+                    {
+
+                        ImGui.SetWindowPos(new Num.Vector2(ImGui.GetWindowPos().X - _adjustX, ImGui.GetWindowPos().Y));
+                    }
+                            
+                }
+                ImGui.Text(_location);
+
+            }
+            ImGui.End();
+            ImGui.PopStyleColor();
 
         }
 
-        public void SaveConfig()
+        private void SaveConfig()
         {
-            Configuration.Enabled = enabled;
-            Configuration.Col = col;
-            Configuration.Scale = scale;
-            Configuration.NoMove = no_move;
-            Configuration.Align = align;
-            this.pi.SavePluginConfig(Configuration);
+            _configuration.Enabled = _enabled;
+            _configuration.Col = _col;
+            _configuration.Scale = _scale;
+            _configuration.NoMove = _noMove;
+            _configuration.Align = _align;
+            _pi.SavePluginConfig(_configuration);
         }
     }
 
@@ -194,8 +191,8 @@ namespace WOLdo
         public bool Enabled { get; set; } = true;
         public ImColor Col { get; set; } = new ImColor { Value = new Num.Vector4(1f,1f,1f,1f) };
         public float Scale { get; set; } = 1f;
-        public bool NoMove { get; set; } = false;
-        public int Align { get; set; } = 0;
+        public bool NoMove { get; set; }
+        public int Align { get; set; }
 
     }
 }
